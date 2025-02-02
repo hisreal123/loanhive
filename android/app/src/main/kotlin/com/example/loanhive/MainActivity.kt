6135dfc97cd61b111498c3bff6d57dcebeddd909
hisreal123 <omosgoerge@gmail.com>
2025-02-02 18:50:41 +0100

    SPLASH SCREEN: is up

---
+++ android/app/src/main/kotlin/com/example/loanhive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.loanhive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/global/global_widget.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+
+// class GlobalWidget extends InheritedElement {
+//   // final ButtonColor buttonColor;
+//   final Widget
+//       logoAsset; // This is the logo asset that we want to share globally
+
+//   const GlobalWidget({ super.key,
+//       required this.buttonColor,
+//       required this.logoAsset,
+//       required super.child});
+
+//   static GlobalWidget? of(BuildContext context) {
+//     return context.dependOnInheritedElement(ancestor: GlobalWidget);
+//   }
+
+//   @override
+//   bool updateShouldNotify(covariant InheritedWidget oldWiget) => false;
+// }
